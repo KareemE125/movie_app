372e32f33216cc480f0fc4c91ad5077f1a01ebe3
KareemE125 <kareemezzat.125@gmail.com>
2023-01-02 20:12:47 +0200

    error fix

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ export default function App()
 //   }
 
   useEffect(() => {
-    init();
+//    init();
     console.log('====================================');
     console.log("DID APP MOUNT");
     console.log('====================================');
